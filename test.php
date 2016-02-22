fe5c7839035c63d2e792b8afdf1127ed83c1ea79
Niels van Velzen <git@hetisniels.nl>
2016-02-22 18:33:21 +0100

    Add more emoticons

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-spl_autoload_register(function ($cls) {
-	$path = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $cls) . '.php';
-
-	if (file_exists($path))
-		require_once $path;
-});
-
-print_r((new \NielsBot\Util\EmojiParser())->parse('Dit is een test :airplane:'));
